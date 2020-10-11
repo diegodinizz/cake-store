f4abcd4c1c1b1d88c381e8ce33119c8ac44273fd
Diego Diniz <diegodinizz@gmail.com>
2020-10-11 14:41:26 +0100

    added aws amplify and configure storage and API

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "cakes-upload"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://q6wwqjqiel.execute-api.eu-west-2.amazonaws.com/api"
+  }
+};
